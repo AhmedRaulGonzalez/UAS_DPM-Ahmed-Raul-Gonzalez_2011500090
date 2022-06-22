4ed977402a374d09333a5bdaf431fe289351bc00
AhmedRaulGonzalez <2011500090@mahasiswa.atmaluhur.ac.id>
2022-06-22 14:43:25 +0700

    UAS DPM

---
+++ app/src/main/java/com/example/uasdpm2011500090/DATADosen.kt
@@ -0,0 +1,11 @@
+package com.example.uasdpm2011500090
+
+class DATADosen(
+    var Nidn: String,
+    var NmDosen: String,
+    var jabatan: String,
+    var GolPangkat: String,
+    var pendidikan: String,
+    var keahlian: String,
+    var ProgramStudi:String
+)
